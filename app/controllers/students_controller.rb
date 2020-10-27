46894a31f4ead7d2626b59911069587d3acb2d91
zach6585 <zmines@wesleyan.edu>
2020-10-27 01:24:56 -0400

    commit tree 198ec459388589eec29a7702b485fa8b54b24f40

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StudentsController < ApplicationController
   
   def index
@@ -12,6 +13,15 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new 
+    puts 'here'
+    @student.first_name = params[:first_name]
+    puts 'here11'
+    @student.last_name = params[:last_name]
+    'here3'
+    @student.save
+    'here5'
+    redirect_to student_path(@student)
   end
 
 end
